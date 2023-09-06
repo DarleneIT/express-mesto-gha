81f8d3132d00b1aa399e8a0ba297612f299b1ed4
Daria <daria.wt@yandex.ru>
2023-09-06 18:58:54 +0300

    14 ПР

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/NotFound');
 const error = require('./middlewares/error');
 
-const { PORT = 3003, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
 
 mongoose.set('strictQuery', true);
