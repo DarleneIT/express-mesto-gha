cf1ce270224e977285fbfe1c6a1eb5f3353f4dc6
Daria <daria.wt@yandex.ru>
2023-09-07 02:35:55 +0300

    14 ПР

--- controllers/cards.js
+++ controllers/cards.js
@@ -90,10 +90,9 @@ module.exports.dislikeCard = (req, res, next) => {
 };
 
 module.exports.deleteCard = (req, res, next) => {
-  const { userId } = req.user;
   Card.findById(req.params.cardId)
     .then((card) => {
-      if (!card.owner.equals(userId._id)) {
+      if (!card.owner.equals(req.user._id)) {
         throw new ForbiddenError('Вы не можете удалить эту карточку');
       }
       Card.deleteOne(card)
